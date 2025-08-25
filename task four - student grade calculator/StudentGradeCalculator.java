e3d0ccee7911deba6d735c117c96b8916de6531b
Bhuvan Asija <139153821+bhuvanasija@users.noreply.github.com>
2025-08-25 21:14:33 +0530

    Update StudentGradeCalculator.java

--- task four - student grade calculator/StudentGradeCalculator.java
+++ task four - student grade calculator/StudentGradeCalculator.java
@@ -5,24 +5,21 @@ public class StudentGradeCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Take number of subjects
         System.out.print("Enter the number of subjects: ");
         int n = sc.nextInt();
 
         int[] marks = new int[n];
         int total = 0;
-
-        // Input marks
         for (int i = 0; i < n; i++) {
             System.out.print("Enter marks of subject " + (i + 1) + " (out of 100): ");
             marks[i] = sc.nextInt();
             total += marks[i];
         }
 
-        // Calculate average percentage
+        
         double percentage = (double) total / n;
 
-        // Assign grade
+    
         char grade;
         if (percentage >= 90) {
             grade = 'A';
@@ -38,7 +35,7 @@ public class StudentGradeCalculator {
             grade = 'F';
         }
 
-        // Display results
+    
         System.out.println("\n--- Result ---");
         System.out.println("Total Marks: " + total + " / " + (n * 100));
         System.out.println("Average Percentage: " + String.format("%.2f", percentage) + "%");
@@ -47,3 +44,4 @@ public class StudentGradeCalculator {
         sc.close();
     }
 }
+
